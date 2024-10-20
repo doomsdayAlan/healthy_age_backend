39554b038b8d7a63b5ba4e752131005dc8917f94
Alan Aguilar <alanalexisaguilar@gmail.com>
2024-10-20 16:34:53 -0600

    Commit Inicial: estado inicial del backend

---
+++ src/main/java/com/healthyage/healthyage/HealthyageApplication.java
@@ -0,0 +1,13 @@
+package com.healthyage.healthyage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyageApplication.class, args);
+	}
+
+}
