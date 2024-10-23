b3c6ff74a0ed8a472c0818456705f607e57d41fc
Alan Aguilar <alanalexisaguilar@gmail.com>
2024-10-22 22:59:40 -0600

    Adición de getters y setters correspondientes a los campos de la clase Notificacion para su manejo con firestore

--- src/main/java/com/healthyage/healthyage/domain/Notificacion.java
+++ src/main/java/com/healthyage/healthyage/domain/Notificacion.java
@@ -26,4 +26,34 @@ public class Notificacion {
     @PropertyName("tipo_notificacion")
     @JsonProperty("tipo_notificacion")
     private String tipoNotificacion;
+
+    @PropertyName("id_notificacion")
+    public String getIdNotificacion() {
+        return idNotificacion;
+    }
+
+    @PropertyName("id_notificacion")
+    public void setIdNotificacion(String idNotificacion) {
+        this.idNotificacion = idNotificacion;
+    }
+
+    @PropertyName("id_tratamiento")
+    public String getIdTratamiento() {
+        return idTratamiento;
+    }
+
+    @PropertyName("id_tratamiento")
+    public void setIdTratamiento(String idTratamiento) {
+        this.idTratamiento = idTratamiento;
+    }
+
+    @PropertyName("tipo_notificacion")
+    public String getTipoNotificacion() {
+        return tipoNotificacion;
+    }
+
+    @PropertyName("tipo_notificacion")
+    public void setTipoNotificacion(String tipoNotificacion) {
+        this.tipoNotificacion = tipoNotificacion;
+    }
 }
