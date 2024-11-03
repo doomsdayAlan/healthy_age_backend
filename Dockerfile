cffa76aa840c0aa96f7964a5bc2bfab55cccae74
Alan Alexis <alanalexisaguilar@gmail.com>
2024-11-03 12:20:58 -0600

    Configurando el proyecto para su despliegue en Render

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,22 @@
-FROM openjdk:17-slim-buster
+# Importing JDK and copying required files
+FROM openjdk:19-jdk AS build
 WORKDIR /app
-COPY build/libs/*.jar app.jar
+COPY pom.xml .
+COPY src src
+
+# Copy Maven wrapper
+COPY mvnw .
+COPY .mvn .mvn
+
+# Set execution permission for the Maven wrapper
+RUN chmod +x ./mvnw
+RUN ./mvnw clean package -DskipTests
+
+# Stage 2: Create the final Docker image using OpenJDK 19
+FROM openjdk:19-jdk
+VOLUME /tmp
+
+# Copy the JAR from the build stage
+COPY --from=build /app/target/*.jar app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
 EXPOSE 8080
-ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/com/healthyage/healthyage/config/OpenAPIConfiguration.java
+++ src/main/java/com/healthyage/healthyage/config/OpenAPIConfiguration.java
@@ -2,6 +2,7 @@ package com.healthyage.healthyage.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +14,17 @@ import io.swagger.v3.oas.models.servers.Server;
 @Configuration
 public class OpenAPIConfiguration {
 
+    @Value("${openapi.server.url}")
+    private String serverUrl;
+
+    @Value("${deployment.environment}")
+    private String deploymentEnvironment;    
+
     @Bean
     public OpenAPI defineOpenApi() {
         Server server = new Server();
-        server.setUrl("http://localhost:8080");
-        server.setDescription("Development");
+        server.setUrl(serverUrl); // Usa la propiedad configurada
+        server.setDescription(deploymentEnvironment);
 
         Contact myContact = new Contact();
         myContact.setName("Alan Alexis Aguilar");
@@ -26,7 +33,8 @@ public class OpenAPIConfiguration {
         Info information = new Info()
                 .title("API para el manejo de la información de la aplicación Healty Age")
                 .version("1.0")
-                .description("Esta api sirve como intermediario para el procesamiento y almacenamiento de la información de la aplicación Healty Age")
+                .description(
+                        "Esta api sirve como intermediario para el procesamiento y almacenamiento de la información de la aplicación Healty Age")
                 .contact(myContact);
         return new OpenAPI().info(information).servers(List.of(server));
     }
